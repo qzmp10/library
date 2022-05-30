4f8cf1584dfce38fd4233bc2fa23b51799f8ea43
qzmp10 <loganrobitaille@outlook.com>
2022-05-30 15:59:47 -0400

    Made form to add book objects to library. Added basic layout for javascript

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //Don't know why not working, will restart
+let addBookBtn = document.querySelector('.add-book');
+
+let bookTitle = document.querySelector('#title');
+let bookAuthor = document.querySelector('#author');
+let bookPages = document.querySelector('#pages');
+let bookRead = document.querySelector('#read');
+
+let library = [];
+
+function Book(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+}
+
+function addBookToLibrary() {
+
+}
+addBookBtn.addEventListener('click', () => {
+
+})
+
